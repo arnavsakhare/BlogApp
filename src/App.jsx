5c74029f8010964f5e712bc6d8c439498461e5fe
Arnav Sakhare <arnav.sakhare123@gmail.com>
2024-11-28 00:12:57 +0530

    authService on first mount to check user

--- src/App.jsx
+++ src/App.jsx
@@ -11,23 +11,26 @@ function App() {
   const dispatch = useDispatch()
 
   useEffect(() => {
-    authService
-      .getCurrentUser()
-      .then((userData) => {
-        if(userData){
-          dispatch(login({userData}))
-        } else {
-          dispatch(logout())
-        }
-      })
-      .finally(() => setLoading(false))
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
   }, [])
+
+  console.log('in app')
 
   return !loading ? (
     <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
       <div className='w-full block'>
-        <Header/>
-        {/* <Outlet /> */}
+        <Header />
+        <main>
+        {/* TODO:  <Outlet /> */}
+        </main>
         <Footer />
       </div>
     </div>
